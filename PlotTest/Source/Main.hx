95941bdfedda51f8663f5dcd60ad632710d0d11f
sakari jokinen <sakariij@gmail.com>
2013-08-05 16:15:48 +0300

    antialiasing settings

--- PlotTest/Source/Main.hx
+++ PlotTest/Source/Main.hx
@@ -3,10 +3,12 @@ package;
 import plot.Plot;
 import flash.display.Sprite;
 import flash.Lib;
+import flash.display.StageQuality;
 
 class Main extends Sprite {
 	public function new () {
  		super ();
+        Lib.current.stage.quality = StageQuality.BEST;
         var plot = new Plot(Lib.current.stage.stageWidth
                             , Lib.current.stage.stageHeight);
         addChild(plot);

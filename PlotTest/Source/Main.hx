c4de986e475db832202f7a02280c9bdb02e77481
sakari jokinen <sakariij@gmail.com>
2013-08-05 23:26:57 +0300

    use normal distribution for test app

--- PlotTest/Source/Main.hx
+++ PlotTest/Source/Main.hx
@@ -6,13 +6,20 @@ import flash.Lib;
 import flash.display.StageQuality;
 
 class Main extends Sprite {
+    function variable(): Float {
+        var p = 0.0;
+        for(i in 0...5) {
+            p += Math.random() * 10.0;
+        }
+        return p / 5;
+    }
 	public function new () {
  		super ();
         Lib.current.stage.quality = StageQuality.BEST;
         var plot = new Plot(Lib.current.stage.stageWidth
                             , Lib.current.stage.stageHeight);
         addChild(plot);
-        var data = [for(i in 0 ... 100000) Math.random() * 10];
+        var data = [for(i in 0 ... 100000) variable()];
         plot.plot(data);
 	}
 }
